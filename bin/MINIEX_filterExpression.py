40c7752cfccae14d52b923e273e6a12a327d7595
SvitlanaLukicheva <svitlana.lukicheva@gmail.com>
2024-11-14 13:38:31 +0100

    Cleaned the code

--- bin/MINIEX_filterExpression.py
+++ bin/MINIEX_filterExpression.py
@@ -1,74 +1,43 @@
-import sys,pandas, collections
+import collections
+import pandas as pd
+import sys
 
-MATRIX=sys.argv[1]
-TF_FILE=sys.argv[2]
-
-CELLS=sys.argv[3]
-
-     
-matrix=pandas.read_csv(MATRIX,sep='\t',index_col=0)
-cells_inMat=list(matrix.columns)
-tf_list=pandas.read_csv(TF_FILE,sep='\t',header=None)[0].to_list()
-
-matrix=matrix[matrix.index.isin(tf_list)] #subset matrix to only keep TFs
+EXPRESSION_MATRIX_FILE  = sys.argv[1]
+TF_LIST_FILE            = sys.argv[2]
+CELLS_TO_CLUSTERS_FILE  = sys.argv[3]
+EXPRESSION_PERCENTAGE   = sys.argv[4]
+CLUSTER_ENRICHMENT_FILE = sys.argv[5]
+OUTPUT_FILE             = sys.argv[6]
 
 
-cell2type={}
-with open(CELLS) as f:
-    for line in f:
-        if line.strip().split('\t')[0] in cells_inMat:
-            cell2type[line.strip().split('\t')[0]]=str(line.strip().split('\t')[1])
+cells_to_clusters_df = pd.read_csv(CELLS_TO_CLUSTERS_FILE, sep='\t', header=None, names=['cell', 'cluster'], dtype={'cluster': str})
+cells_to_clusters_dict = cells_to_clusters_df.set_index('cell')['cluster'].to_dict()
+# how many cells in each cluster; ex: Counter({'clusterA': 150, 'clusterB': 728})
+cells_to_clusters_counter = collections.Counter(list(cells_to_clusters_dict.values()))
 
-cell2type_counter=collections.Counter(list(cell2type.values()))        
+tf_list = pd.read_csv(TF_LIST_FILE, sep='\t', header=None)[0].to_list()
 
-matrix=matrix.transpose()
+expression_matrix_df = pd.read_csv(EXPRESSION_MATRIX_FILE, sep='\t', index_col=0)
+expression_matrix_df = expression_matrix_df[expression_matrix_df.index.isin(tf_list)]  # subset matrix to only keep TFs
+expression_matrix_df = expression_matrix_df.transpose()
+expression_matrix_df = expression_matrix_df[expression_matrix_df.index.isin(cells_to_clusters_dict.keys())]  # subset matrix to only keep annotated cells
+expressed_tfs = list(expression_matrix_df.columns)
 
-matrix=matrix[matrix.index.isin(cell2type.keys())] #subset matrix to only keep annotated cells
-matrix_col=list(matrix.columns)
+# identify valid clusters for each expressed TF (valid = TF is expressed in the specified percentage of cells of that cluster)
+tf_to_valid_clusters_dict = {}
+for tf in expressed_tfs:
+    cells_where_tf_is_expressed = expression_matrix_df[expression_matrix_df[tf] > 1].index.tolist()
+    # in how many cells the TF is expressed for each cluster; ex: Counter({'clusterA': 14, 'clusterB': 240})
+    cells_per_cluster = collections.Counter(cells_to_clusters_dict[cell] for cell in cells_where_tf_is_expressed)
+    # filter clusters based on the expression percentage threshold
+    threshold = lambda cluster: cells_per_cluster[cluster] >= round((int(EXPRESSION_PERCENTAGE) / 100) * cells_to_clusters_counter[cluster])
+    valid_clusters = [cluster for cluster in cells_per_cluster if threshold(cluster)]
+    tf_to_valid_clusters_dict[tf] = valid_clusters if valid_clusters else 'none'
 
-tfsInMatrix=list(set(matrix_col)&set(tf_list))
-
-
-   
-perc=sys.argv[4]   
-tf2clu={}
-
-for tf in tfsInMatrix:
-    cell_tot = collections.Counter([cell2type[i] for i in matrix[matrix[tf]>1].index.tolist() ]) #in how many cells the TF is exp
-    temp=[]
-    for ct in cell_tot:
-        if cell_tot[ct]>= round((int(perc)/100)*cell2type_counter[ct]):
-            temp.append(ct)
-    if len(temp) != 0:
-        tf2clu[tf]=temp
-    else:
-        tf2clu[tf]='none'
-
-
-
-def filtering(inf,out):      
-    fin=open(inf,'r')
-    fout=open(out,'w')
-    dic={}
-    for line in fin:
-        if line.startswith('#') or line.startswith('set_id'): # skip the comments and the header
-            pass
-        else:
-            spl=line.rstrip().rsplit('\t')
-            reg=spl[0]
-            target_genes=spl[9].rsplit(",")
-            ct=spl[1].replace('Cluster_','')
-            if ct in  tf2clu[reg]:
-                dic[reg+'_'+ct]=target_genes
-                
-    for k in dic:
-        dic[k]=list(set(dic[k]))
-                        
-        splK=k.rstrip().rsplit('_')
-        
-        fout.write('_'.join(splK[:-1])+'\tCluster_'+splK[-1]+'\t'+','.join(dic[k])+'\n')
-    fout.close()
-        
-infi = sys.argv[5]
-outi = sys.argv[6]
-filtering(infi,outi)
+# retrieve target genes for pairs (TF, cluster) identified as valid
+cluster_enrichment_df = pd.read_csv(CLUSTER_ENRICHMENT_FILE, sep='\t', comment='#')
+cluster_enrichment_df = cluster_enrichment_df.rename(columns={'set_id': 'TF', 'ftr_id': 'cluster', 'hits': 'target_genes'})
+cluster_enrichment_df['cluster_id'] = cluster_enrichment_df['cluster'].str.split('_', 1).str[1]
+cluster_enrichment_df = cluster_enrichment_df[cluster_enrichment_df.apply(lambda row: row['cluster_id'] in tf_to_valid_clusters_dict.get(row['TF'], []), axis=1)]
+cluster_enrichment_df = cluster_enrichment_df[['TF', 'cluster', 'target_genes']]
+cluster_enrichment_df.to_csv(OUTPUT_FILE, sep='\t', index=None, header=False)
